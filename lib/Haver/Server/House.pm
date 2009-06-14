852d42fe535991e697f3a1b66770fe02275dd495
Dylan William Hardison <dylan@hardison.net>
2009-06-14 14:51:01 +0000

    removed tabs.

--- lib/Haver/Server/House.pm
+++ lib/Haver/Server/House.pm
@@ -6,8 +6,8 @@ use Haver::Server::Room;
 use Tie::CPHash ();
 
 class Haver::Server::House {
-	our $VERSION   = '0.01';
-	our $AUTHORITY = 'cpan:DHARDISON';
+    our $VERSION   = '0.01';
+    our $AUTHORITY = 'cpan:DHARDISON';
 
     foreach my $n (qw( user room )) {
         has "_${n}s" => (
@@ -25,64 +25,64 @@ class Haver::Server::House {
         );
     }
 
-	method new_user(Str $name) {
+    method new_user(Str $name) {
         $self->_set_user(
             $name => Haver::Server::User->new(
                 house => $self,
                 name  => $name,
             )
         );
-	}
+    }
 
-	method new_room(Str $name) {
-		my $room = Haver::Server::Room->new(
-			house => $self,
-			name => $name,
-		);
-		$self->_set_room($name, $room);
+    method new_room(Str $name) {
+        my $room = Haver::Server::Room->new(
+            house => $self,
+            name => $name,
+        );
+        $self->_set_room($name, $room);
 
-		return $room;
-	}
+        return $room;
+    }
 
-	method add_user(Haver::Server::User $user) {
-		$self->_set_user($user->name, $user);
-		$user->attach($self);
-	}
+    method add_user(Haver::Server::User $user) {
+        $self->_set_user($user->name, $user);
+        $user->attach($self);
+    }
 
-	method add_room(Haver::Server::Room $room) {
-		$self->_set_room($room->name, $room);
-		$room->attach($self);
-	}
+    method add_room(Haver::Server::Room $room) {
+        $self->_set_room($room->name, $room);
+        $room->attach($self);
+    }
 
-	method join_room(Str $roomname, Str $username) {
-		my $room = $self->get_room($roomname);
-		my $user = $self->get_user($username);
-		$user->insert($room);
-	}
+    method join_room(Str $roomname, Str $username) {
+        my $room = $self->get_room($roomname);
+        my $user = $self->get_user($username);
+        $user->insert($room);
+    }
 
-	method part_room(Str $roomname, Str $username) {
-		my $room = $self->get_room($roomname);
-		my $user = $self->get_user($username);
-		$user->remove($room);
-	}
+    method part_room(Str $roomname, Str $username) {
+        my $room = $self->get_room($roomname);
+        my $user = $self->get_user($username);
+        $user->remove($room);
+    }
 
-	before delete_user(Str $name) {
-		if ($self->has_user($name)) {
-			my $user = $self->get_user($name);
-			foreach my $room ($user->contents) {
-				$room->remove( $user );
-			}
-			$user->detach;
-		}
-	}
+    before delete_user(Str $name) {
+        if ($self->has_user($name)) {
+            my $user = $self->get_user($name);
+            foreach my $room ($user->contents) {
+                $room->remove( $user );
+            }
+            $user->detach;
+        }
+    }
 
-	before delete_room(Str $name) {
-		if ($self->has_room($name)) {
-			my $room = $self->get_room($name);
-			foreach my $user ($room->contents) {
-				$user->remove( $room );
-			}
-			$room->detach;
-		}
-	}
+    before delete_room(Str $name) {
+        if ($self->has_room($name)) {
+            my $room = $self->get_room($name);
+            foreach my $user ($room->contents) {
+                $user->remove( $room );
+            }
+            $room->detach;
+        }
+    }
 }
