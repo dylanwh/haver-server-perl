082d919558ec7c3fc935656baeae76cffe98c04c
Dylan William Hardison <dylan@hardison.net>
2009-08-09 13:01:45 -0400

    forgot this.

--- lib/Haver/Server.pm
+++ lib/Haver/Server.pm
@@ -5,20 +5,21 @@ use feature ':5.10';
 
 use Haver::Protocol  ();
 use Haver::Server::Handle;
+use Haver::Server::Fail;
+use Haver::Server::Bork;
 
+use AnyEvent ();
 use AnyEvent::Socket ();
-use Tie::CPHash      ();
-use Set::Object      ();
-use Scalar::Util     ();
+use Tie::CPHash  ();
+use Set::Object  ();
+use Scalar::Util ();
 
-class Haver::Server 
-    with MooseX::Runnable
-    with MooseX::Getopt
-{
+
+class Haver::Server with MooseX::Runnable with MooseX::Getopt {
+    use TryCatch;
+
     our $VERSION = '0.01';
     our $AUTHORITY = 'cpan:DHARDISON';
-
-    use TryCatch;
 
     has 'hostname'  => (is => 'ro', isa => 'Str',        required => 1    );
     has 'interface' => (is => 'ro', isa => 'Maybe[Str]', default  => undef);
@@ -62,6 +63,66 @@ class Haver::Server
         },
     );
 
+    has 'roommap' => (
+        traits   => ['NoGetopt'],
+        is       => 'ro',
+        isa      => 'HashRef[Set::Object]',
+        default  => sub { tie my %cphash, 'Tie::CPHash'; $cphash{main} = Set::Object->new;  \%cphash },
+        metaclass => 'Collection::Hash',
+        provides => {
+            set    => 'set_room',
+            get    => 'get_room',
+            exists => 'has_room',
+            delete => 'del_room',
+        },
+    );
+
+    around set_user(Str $name, Haver::Server::Handle $handle) {
+        if (not $self->has_user($name)) {
+            $self->$orig($name, $handle);
+        }
+        else {
+            Haver::Server::Fail->throw(
+                user_exists => $name
+            );
+        }
+    }
+
+    around get_user(Str $name) {
+        if ($self->has_user($name)) {
+            return $self->$orig($name);
+        }
+        else {
+            Haver::Server::Fail->throw(
+                user_not_found => $name
+            );
+        }
+    }
+
+    around set_room(Str $name, $set) {
+        if (not $self->has_room($name)) {
+            $self->$orig($name, $set);
+        }
+        else {
+            Haver::Server::Fail->throw(
+                room_exists => $name
+            );
+        }
+    }
+
+    around get_room(Str $name) {
+        if ($self->has_room($name)) {
+            return $self->$orig($name);
+        }
+        else {
+            Haver::Server::Fail->throw(
+                room_not_found => $name
+            );
+        }
+    }
+
+
+
     method run() {
 	    my $guard = AnyEvent::Socket::tcp_server($self->interface, $self->port, sub { $self->on_connect(@_) });
 
@@ -101,22 +162,6 @@ class Haver::Server
 
         my $e;
         try {
-            given ($handle->phase) {
-                when ('new') {
-                    $self->cleanup($handle) if $name ne 'HAVER';
-                }
-                when ('login') {
-                    if ($name ne 'IDENT') {
-                        $self->bork("expected IDENT");
-                    }
-                }
-                when ('normal') {
-                    if ($name ~~ ['HAVER', 'IDENT']) {
-                        $self->bork("$name only valid during new or login phase.");
-                    }
-                }
-            }
-
             if ($self->can($method_name)) {
                 my $method = $self->meta->get_method($method_name);
                 my @params = $method->_parsed_signature->positional_params();
@@ -126,28 +171,27 @@ class Haver::Server
                     $self->$method_name(@args);
                 }
                 else {
-                    $self->bork("$name requires $required arguments.");
+                    Haver::Server::Bork->throw(
+                        "$name requires $required arguments."
+                    );
                 }
             }
             else {
-                $self->fail('command-not-found');
+                Haver::Server::Fail->throw('command-not-found');
             }
         }
-        catch ($e) {
+        catch (Haver::Server::Fail $e) {
+            my $name = $e->name;
+            $name =~ s/_/-/g;
+            $self->reply(FAIL => $self->current_command, $name, $e->args);
+        }
+        catch (Haver::Server::Bork $e) {
+            $self->reply(BORK => $self->current_command, $e->message);
+        }
+        catch (Item $e) {
             $self->reply(BUG => $self->current_command, $e);
             warn "ERROR: $e";
         }
-    }
-
-    method bork($msg) {
-        $self->reply(BORK => $msg);
-        $self->cleanup($self->current_handle);
-    }
-
-    method fail($name, @msg) {
-        $name =~ s/_/-/g;
-
-        $self->reply(FAIL => $self->current_command, $name, @msg);
     }
 
     method cleanup($handle) {
@@ -164,25 +208,56 @@ class Haver::Server
     }
 
     method cmd_IDENT($name) {
-        if ($self->has_user($name)) {
-            $self->fail(user_exists => $name);
-            return;
-        }
-
-        $self->current_name($name);
         $self->set_user($name => $self->current_handle);
+        $self->current_name($name);
         $self->reply(HELLO => $name);
     }
 
     method cmd_TO($name, $type, @msg) {
-        if (not $self->has_user($name)) {
-            $self->fail(user_not_found => $name);
-            return;
-        }
-
         # a Haver::Server::Handle
         my $user = $self->get_user($name);
         $user->send(FROM => $self->current_name, $type, @msg);
+    }
+
+    method cmd_IN($name, $type, @msg) {
+        my $room   = $self->get_room($name);
+        my $sender = $self->current_handle;
+
+        if (not $room->contains($sender)) {
+            Haver::Server::Fail->throw(not_in_room => $name);
+        }
+
+        foreach my $target ($room->members) {
+            $target->send(IN => $name, $sender->name, $type, @msg);
+        }
+    } 
+
+    method cmd_JOIN($name) {
+        my $room   = $self->get_room($name);
+        my $sender = $self->current_handle;
+
+        if ($room->contains($sender)) {
+            Haver::Server::Fail->throw('already_joined' => $name);
+        }
+
+        $room->insert($sender);
+        foreach my $target ($room->members) {
+            $target->send(JOIN => $name, $sender->name);
+        }
+    }
+
+    method cmd_PART($name) {
+        my $room   = $self->get_room($name);
+        my $sender = $self->current_handle;
+
+        if (not $room->contains($sender)) {
+            Haver::Server::Fail->throw('already_parted' => $name);
+        }
+
+        foreach my $target ($room->members) {
+            $target->send(PART => $name, $sender->name);
+        }
+        $room->remove($sender);
     }
 
 }
